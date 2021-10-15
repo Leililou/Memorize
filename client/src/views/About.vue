9b3ea78c5d60c3c91cd33ea388fa5ad2c9e6003b
leilab <leilab@net.chalmers.se>
2021-10-15 08:39:56 +0200

    add aboutr

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -1,31 +1,51 @@
 <template>
   <div>
-    <div><h3>Introduction to the team.</h3></div>
+    <div><h2>About us and the project</h2></div>
     <div id="about">
       <ul>
-        <li class="bullet-point">The Idea</li>
+        <li class="bullet-point p-3 mb-2 bg-info text-white">The Idea</li>
         <p class="bullet-text">
           The idea came after several attempts to find the perfect project idea to which the technologies, methods and knowledge aquired in the course DIT344
         </p>
-        <li class="bullet-point">The project</li>
+        <li class="bullet-point p-3 mb-2 bg-secondary text-white">The project</li>
         <p class="bullet-text">
-          The project was processed in this order:
+          The project was processed by our team champions in this order:
             <ol>
-              <li>Modeling the ER diagram</li>
-              <li>Create a Backend</li>
-              <li>Create a Frontend</li>
+              <li class="order text-warning">
+              <dt>Modeling the ER diagram:</dt>
+              <dd class="font-weight-light">To structure our work and make the skeleton of our project</dd>
+              </li>
+              <li class="order text-primary"><dt>Create a Backend</dt>
+              <dd class="font-weight-light">Where all the magical stuff happen</dd></li>
+              <dd class="description">Where all the magical stuff happen</dd>
+              <li class="order text-success"><dd>Create a Frontend</dd>
+              <dd class="font-weight-light">Where we see the magic</dd></li>
             </ol>
         </p>
-        <li class="bullet-point">The team</li>
+        <li class="bullet-point p-3 mb-2 bg-warning text-dark">The team</li>
           <p class="bullet-text">Our super team worked on the project as follows:</p>
-            <li>Modeling the ER diagram</li>
-            <li>
-              <dt>Create a Backend</dt>
-              <dd>Where all the magical stuff happen</dd>
-
-            </li>
-            <li>Create a Frontend</li>
+            <li>🤴 Albert</li>
+            <li>👸 Leila</li>
+            <li>🤴 Mattias</li>
       </ul>
+       <p class="test"> .</p>
     </div>
   </div>
 </template>
+
+<style>
+#about{
+  text-align: left;
+  padding-bottom: 70;
+  margin-top: 50px;
+  margin-bottom: 50px;
+  margin-right: 150px;
+  margin-left: 80px;
+}
+li{
+  font-weight: bold;
+}
+h2{
+  margin-top: 70;
+}
+</style>
